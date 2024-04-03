60d453dee1936704fd0b939a2941e11f605fafe5
Agnes <agnes.mccall5@gmail.com>
2024-04-02 21:58:29 -0400

    save board to session

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from boggle import Boggle
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 boggle_game = Boggle()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
 
 @app.route('/')
 def home():
     board = boggle_game.make_board()
-    print(board)
+    print('BOARD:', board)
+    session['board'] = board
+    print('SESSION:', session['board'])
     return render_template('home.html', board=board)
