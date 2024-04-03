3d90dbf048355085642dcad0254fb0e91360d318
Agnes <agnes.mccall5@gmail.com>
2024-04-02 21:48:23 -0400

    show boggle board

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from boggle import Boggle
-from flask import Flask
+from flask import Flask, render_template
 
 boggle_game = Boggle()
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    board = boggle_game.make_board()
+    print(board)
+    return render_template('home.html', board=board)
