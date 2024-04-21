1044c2567d83a090065c208481d42d918e5220c9
Agnes <agnes.mccall5@gmail.com>
2024-04-20 21:35:22 -0400

    add test for home view

--- test.py
+++ test.py
@@ -8,3 +8,15 @@ class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
 
+    def test_home(self):
+        with app.test_client() as client:
+            with client.session_transaction() as change_session:
+                change_session['high_score'] = 3
+                change_session['num_played'] = 4
+
+            resp = client.get("/")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<span id="player-high-score">%s' % session['high_score'], html)
+            self.assertIn('<span id="player-num-played">%s' % session['num_played'], html)
